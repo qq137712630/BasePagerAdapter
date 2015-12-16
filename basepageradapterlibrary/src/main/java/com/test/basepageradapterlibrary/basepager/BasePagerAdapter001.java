291f9b2f109ebe320d0950537d04756aeffa1a59
jqc <1525626676@qq.com>
2015-12-16 15:36:36 +0800

    一个通用的PagerAdapter，代码在basepageradapterlibrary中，主要是仿[base-adapter-helper的RecyclerView版]，例子是[RecyclerTabLayout]

---
+++ basepageradapterlibrary/src/main/java/com/test/basepageradapterlibrary/basepager/BasePagerAdapter001.java
@@ -0,0 +1,89 @@
+package com.test.basepageradapterlibrary.basepager;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by 啟成 on 2015/12/14.
+ */
+public abstract class BasePagerAdapter001<T, H extends BasePagerAdapterHelper> extends PagerAdapter {
+
+
+    protected List<T> data;
+    protected List<String> strPageTitles;
+
+    protected List<Integer> layoutResIds;
+
+    public BasePagerAdapter001(List<String> strPageTitles, List<Integer> layoutResIds) {
+        this(null, strPageTitles, layoutResIds);
+
+    }
+
+    public BasePagerAdapter001(List<T> data, List<String> strPageTitles, List<Integer> layoutResIds) {
+        this.data = data == null ? new ArrayList<T>() : data;
+        this.strPageTitles = strPageTitles;
+        this.layoutResIds = layoutResIds;
+    }
+
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = LayoutInflater.from(container.getContext())
+                .inflate(layoutResIds.get(position % layoutResIds.size()), container, false);
+
+        BasePagerAdapterHelper vh = new BasePagerAdapterHelper(view);
+
+        convert((H) vh, getItem(position));
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+
+        container.removeView((View) object);
+    }
+
+    @Override
+    public String getPageTitle(int position) {
+        return strPageTitles.get(position);
+    }
+
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+
+        return view == object;
+    }
+
+//TODO ==============================================
+
+    public T getItem(int position) {
+        if (position >= data.size()) return null;
+        return data.get(position);
+    }
+
+    /**
+     * Implement this method and use the helper to adapt the view to the given item.
+     *
+     * @param helper A fully initialized helper.
+     * @param item   The item that needs to be displayed.
+     */
+    protected abstract void convert(H helper, T item);
+
+
+    public void addAll(List<T> data) {
+        this.data = new ArrayList<>(data);
+    }
+}
---
+++ demo/src/main/java/com/nshmura/recyclertablayout/demo/basic/DemoBasicActivity.java
@@ -0,0 +1,109 @@
+package com.nshmura.recyclertablayout.demo.basic;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.nshmura.recyclertablayout.RecyclerTabLayout;
+import com.nshmura.recyclertablayout.demo.ColorItem;
+import com.nshmura.recyclertablayout.demo.Demo;
+import com.nshmura.recyclertablayout.demo.DemoColorPagerAdapter;
+import com.nshmura.recyclertablayout.demo.R;
+import com.nshmura.recyclertablayout.demo.utils.DemoData;
+import com.test.basepageradapterlibrary.basepager.BasePagerAdapter;
+import com.test.basepageradapterlibrary.basepager.BasePagerAdapterHelper;
+import com.test.basepageradapterlibrary.basepager.QuickPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class DemoBasicActivity extends AppCompatActivity {
+
+    protected static final String KEY_DEMO = "demo";
+
+    protected RecyclerTabLayout mRecyclerTabLayout;
+
+    public static void startActivity(Context context, Demo demo) {
+        Intent intent = new Intent(context, DemoBasicActivity.class);
+        intent.putExtra(KEY_DEMO, demo.name());
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_basic);
+
+        Demo demo = Demo.valueOf(getIntent().getStringExtra(KEY_DEMO));
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(demo.titleResId);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        List<ColorItem> items = DemoData.loadDemoColorItems(this);
+        Collections.sort(items, new Comparator<ColorItem>() {
+            @Override
+            public int compare(ColorItem lhs, ColorItem rhs) {
+                return lhs.name.compareTo(rhs.name);
+            }
+        });
+
+        DemoColorPagerAdapter adapter = new DemoColorPagerAdapter();
+        adapter.addAll(items);
+
+        List<String> strs = new ArrayList<>();
+        List<Integer> layouts = new ArrayList<>();
+        for (ColorItem mColorItem : items) {
+            strs.add(mColorItem.name);
+        }
+        layouts.add(R.layout.layout_page);
+        BasePagerAdapter baseAdapter = new BasePagerAdapter<ColorItem>(items, strs, layouts) {
+
+            @Override
+            protected void convert(View view, ColorItem item) {
+
+                TextView textView = (TextView) view.findViewById(R.id.title);
+                textView.setText("Page: " + item.hex);
+            }
+        };
+
+        QuickPagerAdapter mQuickPagerAdapter= new QuickPagerAdapter<ColorItem>(items,strs,layouts)
+        {
+
+            @Override
+            protected void convert(BasePagerAdapterHelper helper, ColorItem item) {
+
+                TextView textView =  helper.getTextView(R.id.title);
+                textView.setText("Page: " + item.hex);
+            }
+        };
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+//        viewPager.setAdapter(baseAdapter);
+        viewPager.setAdapter(mQuickPagerAdapter);
+//        viewPager.setAdapter(adapter);
+
+        mRecyclerTabLayout = (RecyclerTabLayout)
+                findViewById(R.id.recycler_tab_layout);
+        mRecyclerTabLayout.setUpWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
